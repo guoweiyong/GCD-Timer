dc9c27188fdd93bc4ba2927a0bd0ac1de3d0b147
guoweiyong <13544004112@163.com>
2020-11-06 20:39:49 +0800

    1.修改提示

--- 内存管理-定时器/内存管理-定时器/GYTimer.h
+++ 内存管理-定时器/内存管理-定时器/GYTimer.h
@@ -11,12 +11,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GYTimer : NSObject
 
+/// 使用该方法不需要时，需要手动停止定时器，不然定时器不会销毁，会一直在内存中执行
+/// @param task <#task description#>
+/// @param start <#start description#>
+/// @param inteval <#inteval description#>
+/// @param repeat <#repeat description#>
+/// @param async <#async description#>
 + (NSString *)executeTask:(void(^)(void))task
                     start:(NSTimeInterval)start
                  interval:(NSTimeInterval)inteval
                    repeat:(BOOL)repeat
                     async:(BOOL)async;
 
+/// 使用该方法不需要时，需要手动停止定时器，不然定时器不会销毁。 并且该定时器对target对象是强引用，如果不手动关闭定时器，那么会导致target对象不会销毁
+/// @param target <#target description#>
+/// @param action <#action description#>
+/// @param start <#start description#>
+/// @param inteval <#inteval description#>
+/// @param repeat <#repeat description#>
+/// @param async <#async description#>
 + (NSString *)executeTaskWithTarget:(id)target
                              action:(SEL)action
                               start:(NSTimeInterval)start
